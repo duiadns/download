3f260723b4d7b1e92a9915833b061e7ee8425d18
stesix <stefano.sicco@gmail.com>
2014-06-18 23:33:46 +0200

    Update duia-bash.sh
    - removed some code repetition
    - added the possibility to define the cache filename (the path)
    - added the possibility to choose the IP version to use
    - added a check for cache file content after it has been set
    
    Tested on a raspberry

--- console-clients/duia-bash.sh
+++ console-clients/duia-bash.sh
@@ -7,62 +7,45 @@ host="example.duia.us" # replace with your own hostname                    #
 md5_pass="000000000000000000000000000000" # replace with your own md5_pass # 
 ############################################################################
 
+use_ip_version=4
+ip_cache_file="duia${use_ip_version}.cache"
+tmp_ip_file="$( mktemp )"
+
 if [ $host == "example.duia.us" ] || [ $md5_pass == "000000000000000000000000000000" ] ; then
- echo "Edit this script and add your own hostname and md5 password!"
+ echo "Edit this script and add your own hostname and md5 password!" >&2
  exit
 fi
 
 ### This script is using "wget". If you prefer "curl" instead, uncomment "curl" lines and comment (#) "wget" ones.
-### IPv4 address update ###
 
-ip4="`wget -qO- http://ipv4.duia.ro`"
-#ip4="`curl -sG http://ipv4.duia.ro`"
+ip="`wget -qO- http://ipv${use_ip_version}.duia.ro`"
+#ip="`curl -sG http://ipv${use_ip_version}.duia.ro`"
 
-echo $ip4 > duia4.last
-if [ -f "duia4.cache" ] ; then
-echo "The file duia4.cache exists"
-n=`grep $ip4 duia4.cache | wc -l`
-if [ $n -eq 0 ] ; then
-echo "Your IPv4 address is not in duia4.cache file; initiate DNS update & duia4.cache file update!"
-`wget -qO- --user-agent="duia-cunix-1.0.0.2" "http://ipv4.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip4=$ip4" > /dev/null`
-#`curl -sG -A "duia-cunix-1.0.0.2" "http://ipv4.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip4=$ip4" > /dev/null`
-cp duia4.last duia4.cache
-rm -f duia4.last
+function set_ip_for_host() {
+	`wget -qO- --user-agent="duia-cunix-1.0.0.2" "http://ipv${use_ip_version}.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip${use_ip_version}=$ip" > /dev/null`
+	#`curl -sG -A "duia-cunix-1.0.0.2" "http://ipv${use_ip_version}.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip${use_ip_version}=$ip" > /dev/null`
+	cp "${tmp_ip_file}" "${ip_cache_file}"
+	rm -f "${tmp_ip_file}"
+	
+	ip_check=$( cat "${ip_cache_file}" )
+	if [ "$ip_check" != "$ip" ] ; then
+		echo "The cache file ${ip_cache_file} does not contain what expected." >&2
+		exit 1
+	fi
+}
+
+echo $ip > $tmp_ip_file
+if [ -f "${ip_cache_file}" ] ; then
+	echo "The file ${ip_cache_file} exists"
+	n=`grep $ip "$ip_cache_file" | wc -l`
+	if [ $n -eq 0 ] ; then
+		echo "Your IPv${use_ip_version} address is not in ${ip_cache_file} file; initiate DNS update & ${ip_cache_file} file update!"
+		set_ip_for_host
+	else
+		echo "Your IPv${use_ip_version} address is in ${ip_cache_file} file; do nothing!"
+		rm -f "${tmp_ip_file}"
+	fi
 else
-echo "Your IPv4 address is in duia4.cache file; do nothing!"
-rm -f duia4.last
-fi
-else
-echo "The duia4.cache file does not exists; initiate DNS update & create duia4.cache file"
-`wget -qO- --user-agent="duia-cunix-1.0.0.2" "http://ipv4.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip4=$ip4" > /dev/null`
-#`curl -sG -A "duia-cunix-1.0.0.2" "http://ipv4.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip4=$ip4" > /dev/null`
-cp duia4.last duia4.cache
-rm -f duia4.last
-fi
-
-### IPv6 address update - delete this part of the script if you don't have an IPv6 address on your router ###
-
-ip6="`wget -qO- http://ipv6.duia.ro`"
-#ip6="`curl -sG http://ipv6.duia.ro`"
-
-echo $ip6 > duia6.last
-if [ -f "duia6.cache" ] ; then
-echo "The file duia6.cache exists"
-m=`grep $ip6 duia6.cache | wc -l`
-if [ $m -eq 0 ] ; then
-echo "Your IPv6 address is not in duia6.cache file; initiate DNS update & duia6.cache file update!"
-`wget -qO- --user-agent="duia-cunix-1.0.0.2" "http://ipv6.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip6=$ip6" > /dev/null`
-#`curl -sG -A "duia-cunix-1.0.0.2" "http://ipv6.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip6=$ip6" > /dev/null`
-cp duia6.last duia6.cache
-rm -f duia6.last
-else
-echo "Your IPv6 address is in duia6.cache file; do nothing!"
-rm -f duia6.last
-fi
-else
-echo "The duia6.cache file does not exists; initiate DNS update & create duia6.cache file"
-`wget -qO- --user-agent="duia-cunix-1.0.0.2" "http://ipv6.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip6=$ip6" > /dev/null`
-#`curl -sG -A "duia-cunix-1.0.0.2" "http://ipv6.duia.ro/dynamic.duia?host=$host&password=$md5_pass&ip6=$ip6" > /dev/null`
-cp duia6.last duia6.cache
-rm -f duia6.last
+	echo "The ${ip_cache_file} file does not exists; initiate DNS update & create ${ip_cache_file} file"
+	set_ip_for_host
 fi
